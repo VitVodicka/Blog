1af014a8fb49f5302cc4069d72f5623e9d57cbb4
Vít Vodička <drakovars1@seznam.cz>
2023-02-15 15:01:34 +0100

    javascript hiding is setted up, recognizing accoriding to the nuber of clicks too, but eventlisteners don t work

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,24 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-document.getElementById("starreview").style.visibility = "hidden";
-// Write your JavaScript code.
+
+function countingNumberOfClicks() {
+    numberOfClicks++;
+    if (numberOfClicks == 4) {
+        starview.visibility = "visible";
+    }
+}
+function hideStar() {
+    starview.style.visibility = "hidden";
+
+}
+let starview = document.getElementById("starreview");
+var button = document.getElementById("javascriptbutton");
+let hideoutButton = document.getElementById("hideoutbutton");
+
+//starview.style.visibility = "hidden";
+let numberOfClicks = 0;
+
+
+
+button.addEventListener('click', countingNumberOfClicks);
+hideoutButton.addEventListener('click', hideStar);
