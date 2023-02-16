be3ea7bd85e88c203b0486e83ec3ab0f38c765d9
Vít Vodička <drakovars1@seznam.cz>
2023-02-16 11:57:26 +0100

    hovering stars work

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -18,20 +18,101 @@ function counting() {
     }
 }
 
-function mouseover(star) {
-    star.innerHTML = "★";
+function mouseover(starNumber) {
+   
+    switch (starNumber) {
+        case 1:
+            star1.innerHTML = "★";
+            star1.style.color ="#22577A";
+            
+            break;
+        case 2:
+            star1.innerHTML = "★";
+            star2.innerHTML = "★";
+            star1.style.color = "#22577A";
+            star2.style.color = "#22577A";
+            break;
+        case 3:
+            star1.innerHTML = "★";
+            star2.innerHTML = "★";
+            star3.innerHTML = "★";
+            star1.style.color = "#22577A";
+            star2.style.color = "#22577A";
+            star3.style.color = "#22577A";
+            break;
+        case 4:
+            star1.innerHTML = "★";
+            star2.innerHTML = "★";
+            star3.innerHTML = "★";
+            star4.innerHTML = "★";
+            star1.style.color = "#22577A";
+            star2.style.color = "#22577A";
+            star3.style.color = "#22577A";
+            star4.style.color = "#22577A";
+            break;
+        case 5:
+            star1.innerHTML = "★";
+            star2.innerHTML = "★";
+            star3.innerHTML = "★";
+            star4.innerHTML = "★";
+            star5.innerHTML = "★";
+            star1.style.color = "#22577A";
+            star2.style.color = "#22577A";
+            star3.style.color = "#22577A";
+            star4.style.color = "#22577A";
+            star5.style.color = "#22577A";
+            break;
+
+    }
+    
 }
 
-function mouseout(star) {
-    star.innerHTML = "☆";
+function mouseout() {
+    star1.innerHTML = "☆";
+    star2.innerHTML = "☆";
+    star3.innerHTML = "☆";
+    star4.innerHTML = "☆";
+    star5.innerHTML = "☆";
+
+    star1.style.color = "black";
+    star2.style.color = "black";
+    star3.style.color = "black";
+    star4.style.color = "black";
+    star5.style.color = "black";
+    
 }
 
-function eventListener(star) {
-    star.addEventListener('mouseover', function () {
-        mouseover(star);
+function eventListener(star1,star2,star3,star4,star5) {
+    star1.addEventListener('mouseover', function () {
+        mouseover(1);
     });
-    star.addEventListener('mouseout', function () {
-        mouseout(star);
+    star2.addEventListener('mouseover', function () {
+        mouseover(2);
+    });
+    star3.addEventListener('mouseover', function () {
+        mouseover(3);
+    });
+    star4.addEventListener('mouseover', function () {
+        mouseover(4);
+    });
+    star5.addEventListener('mouseover', function () {
+        mouseover(5);
+    });
+
+    star1.addEventListener('mouseout', function () {
+        mouseout();
+    });
+    star2.addEventListener('mouseout', function () {
+        mouseout();
+    });
+    star3.addEventListener('mouseout', function () {
+        mouseout();
+    });
+    star4.addEventListener('mouseout', function () {
+        mouseout();
+    });
+    star5.addEventListener('mouseout', function () {
+        mouseout();
     });
     
 }
@@ -51,8 +132,4 @@ let star3 = document.getElementById("hvezdicka3");
 let star4 = document.getElementById("hvezdicka4");
 let star5 = document.getElementById("hvezdicka5");
 
-eventListener(star1);
-eventListener(star2);
-eventListener(star3);
-eventListener(star4);
-eventListener(star5);
+eventListener(star1, star2, star3, star4, star5);
